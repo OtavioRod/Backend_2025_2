0e77fc882702baebf375940421a3fd5173f9c228
OtavioRod <otavioamorimalmeida@hotmail.com>
2025-09-04 20:41:23 -0300

    primeiro commit aula 3

---
+++ todos/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+# 📂 Crie ou edite o arquivo: todos/views.py
+from django.views.generic import ListView
+from .models import Todo
+
+class TodoListView(ListView):
+    model = Todo
+    template_name = "todos/todo_list.html"  # Local do template
+    context_object_name = "todos"           # Nome da lista no template
+
